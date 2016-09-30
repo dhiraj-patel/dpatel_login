f77d2e0a53bd56c3ac915d586cdecb1a0653b0ad
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-09-29 22:15:12 -0400

    cleaned up code

--- app.py
+++ app.py
@@ -12,16 +12,6 @@ def login():
 
 @app.route("/authenticate/", methods = ['POST'])
 def auth():
-    print app
-    print "\n"
-    print request
-    print "\n"
-    print request.headers
-    print "\n"
-    print request.method
-    print "\n"    
-    print request.form['pass']
-    print "\n"
     if(request.form['user'] == "dhiraj" and request.form['pass'] == "patel"):
         return render_template("accept.html",status = "YOU'VE LOGGED IN")
     else:
